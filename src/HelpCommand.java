295fc6ebeaf613e9f2da4266f448239f57a15237
brendanmatulis <31901259+brendanmatulis@users.noreply.github.com>
2018-01-11 12:25:33 -0500

    trying to get it working

--- src/HelpCommand.java
+++ src/HelpCommand.java
@@ -10,7 +10,7 @@ public class HelpCommand extends CommandList {
     public static void run_command() {
         IBIO.output("These are the available commands: ");
 
-        for (i = 0; i < CommandList.length; i = i + 1) {
+        for (int i = 0; i < CommandList.length; i = i + 1) {
             String command_info = String.format("%s -- %s",
                     this.command_name, this.command_description);
             IBIO.output(command_info);
