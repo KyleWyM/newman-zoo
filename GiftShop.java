39e0b25b5c849c6f903f6d060d5fa12ce75f64e0
umbertoborso <Beppiscoresa1>
2018-01-18 11:49:53 -0500

    update of gift shop

--- GiftShop.java
+++ GiftShop.java
@@ -1,7 +1,11 @@
 import ibio.*;
 
 public class GiftShop {
-    public static double foodAndBeverage {
+    static int totalCustomers;
+    totalCustomers = 500;
 
+    public static double foodAndBeverage {
+        int customers = totalCustomers * 0.5;
+        double money = customers
     }
 }
