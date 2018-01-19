dd336a0d0c685ecdfedc431fc2989daa65e5cd46
umbertoborso <Beppiscoresa1>
2018-01-19 11:38:52 -0500

    update of gift shop

--- GiftShop.java
+++ GiftShop.java
@@ -3,9 +3,18 @@ import ibio.*;
 public class GiftShop {
     static int totalCustomers;
     totalCustomers = 500;
+    double totalMoney = 0;
 
     public static double foodAndBeverage {
         int customers = totalCustomers * 0.5;
-        double money = customers
+
+        for (int i = 0; i < customers; i++) {
+            double random = Math.random() * 20;
+            double money = random;
+            totalMoney = totalMoney + money;
+        }
     }
+
+
+
 }
