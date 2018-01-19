5ab4bd03cf2ca185057fff9f1bdb2d070f6decf7
umbertoborso <Beppiscoresa1>
2018-01-19 11:58:42 -0500

    update of gift shop

--- GiftShop.java
+++ GiftShop.java
@@ -3,18 +3,54 @@ import ibio.*;
 public class GiftShop {
     static int totalCustomers;
     totalCustomers = 500;
-    double totalMoney = 0;
+    int totalMoney = 0;
 
     public static double foodAndBeverage {
         int customers = totalCustomers * 0.5;
+        int min = 1;
+        int max = 20;
 
         for (int i = 0; i < customers; i++) {
-            double random = Math.random() * 20;
-            double money = random;
+            int random = min + (int) (Math.random() * ((max - min) + 1));
+            int money = random;
             totalMoney = totalMoney + money;
         }
     }
 
+    public static double zoovenir {
+        int customers = totalCustomers * 0.5;
+        int min = 5;
+        int max = 15;
 
+        for (int i = 0; i < customers; i++) {
+            int random = min + (int) (Math.random() * ((max - min) + 1));
+            int money = random;
+            totalMoney = totalMoney + money;
+        }
+    }
+
+    public static double stuffedWithJoy {
+        int customers = totalCustomers * 0.5;
+        int min = 20;
+        int max = 50;
+
+        for (int i = 0; i < customers; i++) {
+            int random = min + (int) (Math.random() * ((max - min) + 1));
+            int money = random;
+            totalMoney = totalMoney + money;
+        }
+    }
+
+    public static double coffeeShop {
+        int customers = totalCustomers * 0.5;
+        int min = 5;
+        int max = 15;
+
+        for (int i = 0; i < customers; i++) {
+            int random = min + (int) (Math.random() * ((max - min) + 1));
+            int money = random;
+            totalMoney = totalMoney + money;
+        }
+    }
 
 }
