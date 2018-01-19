5c2ddc00c9a26013a199796c9665baf2cc5fd180
KyleWyM <35086398+KyleWyM@users.noreply.github.com>
2018-01-19 11:59:00 -0500

    Add files via upload

---
+++ newman-zoo/src/GiftShop.java
@@ -0,0 +1,20 @@
+import ibio.*;
+
+public class GiftShop {
+    static int totalCustomers;
+    totalCustomers = 500;
+    double totalMoney = 0;
+
+    public static double foodAndBeverage {
+        int customers = totalCustomers * 0.5;
+
+        for (int i = 0; i < customers; i++) {
+            double random = Math.random() * 20;
+            double money = random;
+            totalMoney = totalMoney + money;
+        }
+    }
+
+
+
+}
