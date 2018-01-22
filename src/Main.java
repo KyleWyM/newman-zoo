9b526316c32c9874db39d9e1d7dd4d40bd48ba87
Picapau99 <martin.lentz999@gmail.com>
2018-01-22 11:25:22 -0500

    Original code pasted into new java class, will work on rewriting into this code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main{
+    public static int cows = 0;
+    public static int pigs = 0;
+    public static int chickens = 0;
+    public static boolean buyingAnimals;
+    public static int cashMoney = 1000;
     public static void main(String args[]){
 
     }
