7c70b7fb62122770e911c9e5d872453d42d14723
Picapau99 <martin.lentz999@gmail.com>
2018-01-22 08:32:08 -0500

    Original code pasted into new java class, will work on rewriting into this code

---
+++ src/Buyer.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Buyer {
+    public static void buyAnimal(){
+        while (Main.buyingAnimals == true) {
+            Scanner user_input = new Scanner(System.in);
+            System.out.println("What animal would you like to buy: Cows = 1 , Pigs = 2 , Chickens = 3, Done = 4");
+            int decisions = user_input.nextInt();
+            if (decisions == 1) {
+                System.out.println("How many Cows would you like to buy");
+                int tempcows = user_input.nextInt();
+                int priceofcows = tempcows * 200;
+                if (priceofcows > Main.cashMoney) {
+                    System.out.println("You don't have that kind of money");
+                } else {
+                    Main.cows = tempcows + Main.cows;
+                    Main.cashMoney = Main.cashMoney - priceofcows;
+                    System.out.println("You now own " + Main.cows + " cows");
+                    System.out.println("Money = " + Main.cashMoney);
+
+                }
+            } else {
+                if (decisions == 2) {
+                    System.out.println("How many Pigs would you like to buy");
+                    int temppigs = user_input.nextInt();
+                    int priceofpigs = temppigs * 100;
+                    if (priceofpigs > Main.cashMoney) {
+                        System.out.println("You don't have that kind of money");
+                    } else {
+                        Main.pigs = temppigs + Main.pigs;
+                        Main.cashMoney = Main.cashMoney - priceofpigs;
+                        System.out.println("You now own " + Main.pigs + " Pigs");
+                        System.out.println("Money = " + Main.cashMoney);
+
+                    }
+                } else {
+                    if (decisions == 3) {
+                        System.out.println("How many Chickens would you like to buy");
+                        int tempchickens = user_input.nextInt();
+                        int priceofchickens = tempchickens * 50;
+                        if (priceofchickens > Main.cashMoney) {
+                            System.out.println("You don't have that kind of money");
+                        } else {
+                            Main.chickens = tempchickens + Main.chickens;
+                            Main.cashMoney = Main.cashMoney - priceofchickens;
+                            System.out.println("You now own " + Main.chickens + " chickens");
+                            System.out.println("Money = " + Main.cashMoney);
+                        }
+
+                    } else {
+                        if (decisions == 4) {
+                            Main.buyingAnimals = false;
+                        } else {
+                            System.out.println("you fucked up somewhere");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+}
