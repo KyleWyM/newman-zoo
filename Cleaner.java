0734ca98b59a8e95728b790232e3bfd0b07dc92f
Superman0101 <35230882+Superman0101@users.noreply.github.com>
2018-02-08 11:35:28 -0500

    Add files via upload

---
+++ Cleaner.java
@@ -0,0 +1,12 @@
+import ibio.IBIO;
+
+public class Cleaner extends Employee {
+
+    public Cleaner(String name, int experience, double salary) {
+        super(name, 0, 30);
+    }
+
+    //methods
+
+
+}
---
+++ GiftShopE.java
@@ -0,0 +1,15 @@
+import ibio.*;
+
+public class GiftShopE extends Employee {
+
+    String name;
+    int experience;
+    double salary;
+
+    public GiftShopE (String name, int experience, double salary) {
+        super(name, experience, salary);
+        this.name = name;
+        this.experience = experience;
+        this.salary = salary;
+    }
+}
