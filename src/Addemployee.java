39fef02a0139cd22633f61424864c56f282407c6
Aidan spengler <ajax@Aidans-MacBook-Pro.local>
2018-03-09 09:35:36 -0500

    more merge fixes

---
+++ src/Addemployee.java
@@ -0,0 +1,6 @@
+public class Addemployee {
+    public static void addEmployee(){
+        System.out.print("an employee has been added");
+
+    }
+}
---
+++ src/ListEmployees.java
@@ -0,0 +1,16 @@
+public class ListEmployees {
+    public static void myList(){
+        System.out.print("*list of employees*");
+    }
+    public static void listEmpoyees(){
+
+        String numEmployeeMsg;
+        int numEmployees;
+
+        numEmployees = 0; // todo this will be the length of the array list
+        myList();
+        numEmployeeMsg = String.format("you have %s employees",numEmployees);
+
+        System.out.print(numEmployeeMsg);
+    }
+}
