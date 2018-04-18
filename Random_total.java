5331b0a0c290b9863552ca853545f17eabc0e914
ZeJunWang0905 <35231064+ZeJunWang0905@users.noreply.github.com>
2018-04-18 12:54:40 -0400

    Add files via upload

---
+++ Random_total.java
@@ -0,0 +1,98 @@
+import ibio.*;
+public class Random_total {
+    double Animal_number;
+    double total_money;
+
+    public Random_total(double animal_number, double total_money, double people_number, double empley, String player) {
+        Animal_number = animal_number;
+        this.total_money = total_money;
+        this.people_number = people_number;
+        this.empley = empley;
+        this.player = player;
+//        System.out.println("msesgagsgs");
+    }
+
+    double people_number;
+    double empley;
+    public Random_total(String player) {
+        this.player = player;
+    }
+
+    String player;
+
+
+    Random2 Animal_escape= new Random2(-1,total_money-1000,-0,-0);
+    Random2 haramba_came =new Random2(+1,+100,+0,+0);
+    Random2 emply_sick =new Random2(+0,-10000,-1,+0);
+    Random2 earth_quick= new Random2(-6,-100000,-0,-5);
+    Random2 passgo = new Random2(+0,+5000,+0,+0);
+    Random2 Animal_merdar = new Random2(-1,-3000,+0,-1);
+    Random2 Vampire = new Random2(-0,-5000,+0,-3);
+    Random2 Company_invest = new Random2(-2,+500000,+0,+0);
+        public static void main(String args[]){
+            int random = (int )(Math.random() * 1000 + 0.001);
+            boolean animalEscape = (random < 1000);
+            if (animalEscape =(random<200)){
+            String j="There was an animal escape ! You lost 1000 dollor";
+            IBIO.output(j);
+            System.out.println();
+            }
+
+            boolean haramba_come = (random < 10);
+            if (haramba_come =(random<300)) {
+                String k = "The Haramba has come alive!!!!You gain 100 dollor,1 animal";
+                IBIO.output(k);
+                System.out.println();
+
+
+            }
+
+            boolean emply_sick = (random < 200);
+            if (emply_sick =(random<200)) {
+                String q = "One of your employ get sick and died";
+                IBIO.output(q);
+                System.out.println();
+            }
+
+            boolean earth_quick = (random<5);
+            if (earth_quick= (random<5)) {
+                String y= "There was a massaive earth quick!";
+                IBIO.output(y);
+                System.out.println();
+            }
+
+            boolean passgo = (random<100);
+            if (passgo=(random<100)){
+                String c = "You hit the passgo! gaing 5000 dollar";
+                IBIO.output(c);
+                System.out.println();
+            }
+
+            boolean Animal_merdar = (random<100);
+            if (passgo=(random<100)){
+                String j = "Your employ killed a animal in your zoo!!!!! You lost 1 animal and 1 employ,total lost 3000 dollar";
+                IBIO.output(j);
+                System.out.println();
+            }
+
+            boolean Vampire= (random<50);
+            if (Vampire=(random<50)){
+                String m = "The vampair killed 3 of your worker, you lost 3 employe and 5000 dollar";
+                IBIO.output(m);
+                System.out.println();
+            }
+
+            boolean Company_invest = (random <125);
+            if (Company_invest=(random<125)){
+                String l = "The Restrant 'I don't care' invest The newman Zoo, but in exchange lost 2 tigers and 2 Lions";
+                IBIO.output(l);
+                System.out.println();
+            }
+
+
+
+
+
+        }
+
+}
