87e8b8b8606974867933e15744280921bc705ea7
ZeJunWang0905 <35231064+ZeJunWang0905@users.noreply.github.com>
2018-04-18 12:56:23 -0400

    Add files via upload

---
+++ src/Random2.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Random2 {
+    double Animal_number;
+    double total_money;
+    double people_number;
+    double empley;
+
+    public Random2(double animal_number, double total_money, double people_number, double empley) {
+        Animal_number = animal_number;
+        this.total_money = total_money;
+        this.people_number = people_number;
+        this.empley = empley;
+
+    }
+
+    double max =1000;
+    double min =0.001;
+
+
+
+}
