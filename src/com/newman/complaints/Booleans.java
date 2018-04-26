7f8b40b02846eaf9b9df2aaf494088fa578ff35c
brendanmatulis <31901259+brendanmatulis@users.noreply.github.com>
2018-04-26 01:19:18 -0400

    Finally fixed merge and am uploading the complaints

---
+++ src/com/newman/complaints/Booleans.java
@@ -0,0 +1,224 @@
+package com.newman.complaints;
+
+import com.newman.animals.ManageAnimals;
+import com.newman.animals.species.Animal;
+import com.newman.game.DataCalculations;
+import com.newman.game.TicketAlgorithm;
+import com.newman.player.PlayerStats;
+import ibio.IBIO;
+
+import java.util.ArrayList;
+
+public class Booleans {
+    //TODO: save innitial variables and complaints_initialized
+    //TODO: add a complaints list in report
+
+    private static boolean complaints_initialized = false;
+
+    //Animal related booleans
+    static boolean too_many_mammals;
+    static boolean too_few_reptiles;
+    static boolean too_few_birds;
+    static boolean too_many_animals_too_fast;
+    static boolean avian_bonus;
+    static boolean reptile_bonus;
+    static boolean mammal_bonus;
+    static boolean too_many_of_one_animal;
+    static boolean too_many_carnivores;
+
+
+    static boolean owner_is_rich;
+    static boolean ticket_prices_fluctuating;
+    static boolean reputation_dropping;
+    static boolean too_crowded;
+    static boolean too_sparse;
+    static boolean increasing_popularity;
+
+    private static int initial_money;
+    private static int change_in_money;
+
+    private static int initial_reputation;
+    private static int change_in_reputation;
+
+    private static int initial_ticket_prices;
+    private static int change_in_ticket_prices;
+
+    private static int initial_visitors;
+    private static int change_in_visitors;
+
+    //Animal data
+    private static int initial_num_animals;
+    private static int change_in_num_animals;
+
+    static int num_reptiles;
+    private static int num_mammals;
+    static int num_birds;
+    private static int num_carnivores;
+
+    private static String[] reptiles = {"Dragon", "Snake"};
+    private static String[] birds = {"Flamingo", "Ostrich"};
+    private static String[] carnivores = {"Dragon", "Lion", "Snake", "Tiger"};
+
+
+    static void update_variables() {
+        resetBooleans();
+        if (!complaints_initialized)
+            initialize_variables();
+
+        calculate_changes();
+        analyze_arrays();
+        configure_booleans();
+    }
+
+    private static void initialize_variables() {
+        initial_money = PlayerStats.money;
+        initial_ticket_prices = TicketAlgorithm.ticket_price;
+        initial_reputation = PlayerStats.reputation;
+        initial_num_animals = PlayerStats.myAnimals.size();
+        initial_visitors = DataCalculations.visitors;
+    }
+
+    private static void calculate_changes() {
+        if (PlayerStats.dayNum % 3 == 0) {
+            //Every three days, the changes in variables are counted.
+            change_in_money = PlayerStats.money - initial_money;
+            change_in_ticket_prices = TicketAlgorithm.ticket_price - initial_ticket_prices;
+            change_in_reputation = PlayerStats.reputation - initial_reputation;
+            change_in_num_animals = PlayerStats.myAnimals.size() - initial_num_animals;
+            change_in_visitors = DataCalculations.visitors - initial_visitors;
+
+            initialize_variables();
+        }
+    }
+
+    private static void analyze_arrays() {
+        num_reptiles = count_animal_matches(PlayerStats.myAnimals, reptiles);
+        num_birds = count_animal_matches(PlayerStats.myAnimals, birds);
+        num_mammals = PlayerStats.myAnimals.size() - num_reptiles - num_birds;
+        num_carnivores = count_animal_matches(PlayerStats.myAnimals, carnivores);
+    }
+
+    private static int count_animal_matches(ArrayList<Animal> myAnimals, String[] animal_array) {
+        String myAnimal;
+        int num_matches = 0;
+
+        for (int i = 0; i < myAnimals.size(); i++) {
+            myAnimal = myAnimals.get(i).getClass().getSimpleName();
+
+            for (String test_animal : animal_array) {
+                if (myAnimal.equals(test_animal))
+                    num_matches++;
+            }
+        }
+
+        return num_matches;
+    }
+
+    private static void too_many_of_an_animal() {
+        int max_species_index = 0;
+        int max_num_species = 0;
+        String mySpeciesName, testSpeciesName;
+
+        int num_species;
+        for (int i = 0; i < ManageAnimals.available_animals.size(); i++) {
+            num_species = 0;
+            testSpeciesName = ManageAnimals.available_animals.get(i).getClass().getSimpleName();
+            for (int k = 0; k < PlayerStats.myAnimals.size(); k++) {
+               mySpeciesName = PlayerStats.myAnimals.get(k).name;
+               if (mySpeciesName.equals(testSpeciesName))
+                   num_species++;
+            }
+
+            if (num_species > max_num_species) {
+                max_num_species = num_species;
+                max_species_index = i;
+            }
+        }
+
+        double species_abundance_ratio = (double) max_num_species / (double) PlayerStats.myAnimals.size();
+        if (PlayerStats.myAnimals.size() > 5 && species_abundance_ratio > 0.5) {
+            mySpeciesName = ManageAnimals.available_animals.get(max_species_index).getClass().getName();
+            IBIO.output("More than half your animals are " + mySpeciesName + "s!" +
+                    "\nCheck your report to see the consequences!");
+            too_many_of_one_animal = true;
+        }
+    }
+
+    private static void resetBooleans() {
+        too_many_mammals = false;
+        too_few_reptiles = false;
+        too_few_birds = false;
+        too_many_animals_too_fast = false;
+        avian_bonus = false;
+        reptile_bonus = false;
+        mammal_bonus = false;
+        too_many_of_one_animal = false;
+        too_many_carnivores = false;
+        owner_is_rich = false;
+        ticket_prices_fluctuating = false;
+        reputation_dropping = false;
+        too_crowded = false;
+        too_sparse = false;
+        increasing_popularity = false;
+    }
+
+    private static void configure_booleans() {
+        //Animal related booleans:
+        too_many_of_an_animal();
+        double numAnimals = (double) PlayerStats.myAnimals.size();
+
+        boolean sufficient_animals = PlayerStats.myAnimals.size() >= 4;
+        //Special bonuses that come from different percentages of animals
+        double percentage_reptiles = ((double) num_reptiles / numAnimals);
+        double percentage_mammals = ((double) num_mammals / numAnimals);
+        double percentage_birds = ((double) num_birds / numAnimals);
+        double percentage_carnivores = ((double) num_carnivores / numAnimals);
+
+        if (sufficient_animals) {
+            if (percentage_reptiles > 0.10) {
+                //If more than 10% of a players animals are reptiles, they get the reptile bonus
+                reptile_bonus = true;
+            } else if (percentage_reptiles < 0.05) {
+                too_few_reptiles = true;
+            }
+            if (percentage_mammals > 0.80) {
+                //If more than 80% of a players animals are reptiles, they get the reptile bonus
+                mammal_bonus = true;
+            } else if (percentage_mammals > 0.90) {
+                too_many_mammals = true;
+            }
+            if (percentage_birds > 0.10) {
+                //If more than 10% of a players animals are birds, they get the avian bonus
+                avian_bonus = true;
+            } else if (percentage_birds < 0.05) {
+                too_few_birds = true;
+            }
+            if (percentage_carnivores > 0.50) {
+                too_many_carnivores = true;
+            }
+        }
+
+        if ((double) change_in_num_animals > 0.25 * numAnimals) {
+            too_many_animals_too_fast = true;
+        }
+
+        //Other booleans:
+        if ((double) change_in_money > 0.15 * (double) PlayerStats.money) {
+            owner_is_rich = true;
+        }
+        if ((double) DataCalculations.visitors > numAnimals * 10) {
+            too_crowded = true;
+        } else if ((double) DataCalculations.visitors < numAnimals) {
+            too_sparse = true;
+        }
+        if ((double) change_in_ticket_prices / (double) initial_ticket_prices > 1.00) {
+            ticket_prices_fluctuating = true;
+        }
+        if ((double) change_in_reputation < 0) {
+            reputation_dropping = true;
+        }
+        if ((double) change_in_visitors / (double) initial_visitors > 0.20) {
+            increasing_popularity = true;
+        }
+    }
+}
---
+++ src/com/newman/complaints/ComplaintChecker.java
@@ -0,0 +1,118 @@
+package com.newman.complaints;
+
+import com.newman.player.PlayerStats;
+import ibio.IBIO;
+
+import java.util.ArrayList;
+
+public class ComplaintChecker {
+    private static ArrayList<String> complaints = new ArrayList<>();
+
+    public static void listComplaints() {
+        IBIO.output("Complaints:\n");
+        for (int i = 0; i < complaints.size(); i++) {
+            String message = String.format( "%2d. %-20s",
+                    i + 1, complaints.get(i));
+            IBIO.output(message + "\n");
+        }
+        complaints = new ArrayList<>();
+    }
+
+    public static void update() {
+        Booleans.update_variables();
+
+        String complaint;
+        //Animal related complaints:
+        if (Booleans.too_many_mammals) {
+            PlayerStats.reputation--;
+            complaint = "You are losing reputation because you have to many mammals! Get more diversity!";
+            complaints.add(complaint);
+        }
+        if (Booleans.too_few_reptiles) {
+            complaint = "You have too few reptiles. Get more to get the reptile bonus.";
+            complaints.add(complaint);
+        }
+        if (Booleans.too_few_birds) {
+            complaint = "You're losing money because there aren't a lot of birds so there's environmental " +
+                    "\ntrouble or something. Basically, get more birds...";
+            PlayerStats.money--;
+            complaints.add(complaint);
+        }
+        if (Booleans.too_many_animals_too_fast) {
+            complaint = "You're buying animals very fast! You get a bonus equal to " + PlayerStats.myAnimals.size() + "!";
+            PlayerStats.money += PlayerStats.myAnimals.size();
+            complaints.add(complaint);
+        }
+        if (Booleans.avian_bonus) {
+            complaint = "You have the avian bonus because of your bird collection! " +
+                    "\nGet a bonus of money equal to 5 for every bird you have!";
+            PlayerStats.money += Booleans.num_birds * 5;
+            PlayerStats.reputation += 10;
+            complaints.add(complaint);
+        }
+        if (Booleans.reptile_bonus) {
+            complaint = "You have the reptile bonus because of your reptile collection! " +
+                    "\nGet a bonus of money equal to 5 for every reptile you have!";
+            PlayerStats.money += Booleans.num_reptiles * 5;
+            complaints.add(complaint);
+        }
+        if (Booleans.mammal_bonus) {
+            complaint = "You have the avian bonus because of your reptile collection! " +
+                    "\nGet a bonus of money equal to 5 per turn!";
+            PlayerStats.money += 5;
+            complaints.add(complaint);
+        }
+        if (Booleans.too_many_of_one_animal) {
+            complaint = "People think your a fraud because you are ripping people off by " +
+                    "\nshowing the same type of animal over and over. People are protesting" +
+                    "\nagainst you in the thousands and maybe even hundreds of thousands." +
+                    "\nYou lose 1 reputation each turn!!!!";
+            PlayerStats.money--;
+            complaints.add(complaint);
+        }
+        if (Booleans.too_many_carnivores) {
+            complaint = "You have too many carnivores! They are rampaging the zoo, leaving the bodies of" +
+                    "\nvisitors strewn all about. It's carnage. The chances of survival are remote." +
+                    "\nYou must pay $3 in reparations. Also, you have bad Yelp reviews now, so " +
+                    "\nyou will lose 1 reputation per turn.";
+            PlayerStats.money -= 3;
+            PlayerStats.reputation--;
+            complaints.add(complaint);
+        }
+        if (Booleans.owner_is_rich) {
+            int bonus = (int) ((double) PlayerStats.money * 0.9);
+            complaint = "You are rolling in cash! Get a bonus of " + bonus + " this turn! But you will lose reputation.";
+            PlayerStats.money += bonus;
+            PlayerStats.reputation -= 9;
+            complaints.add(complaint);
+        }
+        if (Booleans.ticket_prices_fluctuating) {
+            complaint = "Your ticket prices are fluctuating, so you will lose 5 reputation.";
+            PlayerStats.reputation -= 5;
+            complaints.add(complaint);
+        }
+        if (Booleans.reputation_dropping) {
+            complaint = "Your reputation is dropping! It's building momentum and now you will lose " +
+                    "\neven more reputation!";
+            PlayerStats.reputation -= 5;
+            complaints.add(complaint);
+        }
+        if (Booleans.too_crowded) {
+            complaint = "Your zoo is two crowded. One guy even fell in the " + PlayerStats.myAnimals.get(0) + " " +
+                    "\nexhibit. Lose $5 and 1 reputation until your zoo is no longer crowded.";
+            PlayerStats.reputation--;
+            PlayerStats.money -= 5;
+            complaints.add(complaint);
+        }
+        if (Booleans.too_sparse) {
+            complaint = "Get more animals!";
+            complaints.add(complaint);
+        }
+        if (Booleans.increasing_popularity) {
+            complaint = "Your growing in popularity! Get 15 reputation and $30!";
+            PlayerStats.money += 30;
+            PlayerStats.reputation += 15;
+            complaints.add(complaint);
+        }
+    }
+}
