0247a3ecd86480f7e5be90eb13863f308d550a9f
Kyle <kmcfatter19@newmanstudents.org>
2018-04-25 20:12:55 -0400

    added new employees

--- src/com/newman/employees/Employee.java
+++ src/com/newman/employees/Employee.java
@@ -21,9 +21,15 @@ public class Employee implements Serializable {
     public static Employee reluctant_teenager = new Employee("Reluctant Teenager", 1, 5, 1);
     static Employee average_teenager = new Employee("Average Teenager", 2, 7, 2);
     static Employee nerdy_teenager = new Employee("Nerdy Teenager", 3, 9, 2);
+    static Employee undergrad_student = new Employee("Undergraduate Student", 4, 12, 3);
+    static Employee biology_major = new Employee("Biology Major", 5, 15, 4);
+    static Employee zoologist = new Employee("Zoologist", 7, 20, 5);
+    static Employee martin = new Employee("Martin Lentz", 0, 1, 6);
     static Employee steve_irwin = new Employee("Steve Irwin", 100, 1000, 10);
+    static Employee father_luca = new Employee("Father Luca", 10000, 1000000000, 11);
     public static Employee[] employee_list = new Employee[] {
-      reluctant_teenager, average_teenager, nerdy_teenager, steve_irwin,
+      reluctant_teenager, average_teenager, nerdy_teenager, undergrad_student, biology_major, zoologist, martin,
+            steve_irwin, father_luca
     };
     public static ArrayList<Employee> available_employees = new ArrayList<Employee>();
 }
